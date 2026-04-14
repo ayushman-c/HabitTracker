7b30e02284f223762baa1875104b75620aef847b
ayushman-c <ayushmanc67@gmail.com>
2026-04-14 11:20:15 +0530

    added sign-in page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Routes, Route } from 'react-router'
 import LandingPage from './pages/LandingPage'
+import Signin from './pages/Signin'
 
 const App = () => {
   return (
-    <>
-      <LandingPage/>
-    </>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/sign-in" element={<Signin />} />
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/Signin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Signin.css'
+import Navbar2 from '../components/SigninC/Navbar2'
+import Form from '../components/SigninC/Form'
+
+const Signin = () => {
+  return (
+    <>
+        <Navbar2/>
+        <Form/>
+    </>
+  )
+}
+
+export default Signin
